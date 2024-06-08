80e71a71efe68792329e5011a2733c6fd781c4f7
Alexander <alexzavr003@gmail.com>
2024-06-09 00:16:03 +0300

    Сверстал страницу логина с рабочей кнопкой перехода в акк (без необходимости авторизации - чисто визуал)

--- website_project/login_app/views.py
+++ website_project/login_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Main Page!")
+    return render(request, "login_app/index.html")
